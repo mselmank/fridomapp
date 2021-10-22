229e4ffe79eda45103a60744eb8dc1f0a55b58f8
mselmank <48356632+mselmank@users.noreply.github.com>
2021-10-22 19:43:06 -0300

    Update next.config.js

--- fridomapp/next.config.js
+++ fridomapp/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   serverRuntimeConfig: {
-      secret: ''
+      secret: 'Esto es un secreto'
   },
   publicRuntimeConfig: {
       apiUrl: process.env.NODE_ENV === 'development'
